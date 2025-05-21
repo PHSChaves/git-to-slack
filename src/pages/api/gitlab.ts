d614062f01b8aa42c0382181027546f6fdf7f24c
PSChaves <paulo.chaves@usereserva.com>
2025-05-21 12:22:19 -0300

    make several changes

--- src/pages/api/gitlab.ts
+++ src/pages/api/gitlab.ts
@@ -1,17 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-const SLACK_WEBHOOK = process.env.SLACK_WEBHOOK_URL!;
+const SLACK_WEBHOOK = process.env.SLACK_WEBHOOK_URL;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') return res.status(405).end('Method Not Allowed');
 
+  if (!SLACK_WEBHOOK) {
+    console.error("Webhook URL não definida");
+    return res.status(500).end('Slack webhook não configurado');
+  }
+
   const mr = req.body?.object_attributes;
 
-  if (!mr) return res.status(400).send('No MR payload');
+  if (!mr) return res.status(400).send("Payload inválido");
 
   if (mr.merge_status === 'cannot_be_merged') {
     const message = {
-      text: `🚨 MR com conflito!\n*${mr.title}*\n🔗 <${mr.url}|Abrir MR>`,
+      text: `🚨 Merge Request com conflito:\n*${mr.title}*\n🔗 <${mr.url}|Ver MR>`,
     };
 
     await fetch(SLACK_WEBHOOK, {
